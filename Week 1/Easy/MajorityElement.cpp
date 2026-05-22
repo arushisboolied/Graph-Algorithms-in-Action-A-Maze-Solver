cb7f35c25e705b2041a3550120d9b188e3015248
Arush Anand <arushanand107@gmail.com>
2026-05-22 20:59:37 +0530

    Solved Majority Element

---
+++ Week 1/Easy/MajorityElement.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int ans = 0, freq = 0;
+        for(int val : nums){
+            if(freq == 0) ans = val;
+            if(ans == val) freq++;
+            else freq--;
+        }
+        return ans;
+    }
+};
