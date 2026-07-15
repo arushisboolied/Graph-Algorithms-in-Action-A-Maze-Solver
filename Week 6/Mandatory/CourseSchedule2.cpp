a1b07b9c672a20c60b11e110b52abee34d7ba544
Arush Anand <arushanand107@gmail.com>
2026-07-15 23:54:35 +0530

    Solved Course Schedule 2

---
+++ Week 6/Mandatory/CourseSchedule2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prereqs) {
+        vector<vector<int>> adj(numCourses);
+        vector<int> indeg(numCourses, 0);
+        for (auto& p : prereqs) {
+            adj[p[1]].push_back(p[0]);
+            indeg[p[0]]++;
+        }
+        queue<int> q;
+        for (int u = 0; u < numCourses; u++)
+            if (indeg[u] == 0)
+                q.push(u);
+        vector<int> order;
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            order.push_back(u);
+            for (int v : adj[u])
+                if (--indeg[v] == 0)
+                    q.push(v);
+        }
+        return (int)order.size() == numCourses ? order : vector<int>{};
+    }
+};
